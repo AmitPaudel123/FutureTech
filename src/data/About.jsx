48e3b18370bc21b4c910d02bbdb9f93ea1295a55
rajan <rajanshahi1267@gmail.com>
2024-02-04 11:07:36 -0700

    add

---
+++ src/data/About.jsx
@@ -0,0 +1,30 @@
+import scrool1 from "../assets/scrools/scrool1.webp";
+import scrool2 from "../assets/scrools/scrool2.webp";
+import scrool3 from "../assets/scrools/scrool3.webp";
+
+export const items = [
+  {
+    img: scrool1,
+    name: "Rajan Bahadur Shahi",
+    work: "English Teacher.",
+    desc: "He charms of pleasure of the moment, so blinded by desire foresee.",
+  },
+  {
+    img: scrool2,
+    name: "Lekendra Chaulagain",
+    work: "Software Teacher.",
+    desc: "To take a teachings, which of us ever undertakes physical exercise.",
+  },
+  {
+    img: scrool3,
+    name: "Gita Bista",
+    work: "Dancer Teacher.",
+    desc: "The actual teachings of the great explorer of the master-builder.",
+  },
+  {
+    img: scrool1,
+    name: "Hemraj Thapa",
+    work: "Maths Teacher.",
+    desc: "I must explain to you how all this idea of denouncing pleasure.",
+  },
+];
