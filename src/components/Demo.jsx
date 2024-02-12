b0d7a196fc272103d7e784c29287cd59bd645bd5
rajan <rajanshahi1267@gmail.com>
2024-02-12 10:17:46 -0700

    add demo file

---
+++ src/components/Demo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Demo() {
+  return (
+    <div>
+      <div className="  relative    bg-gradient-to-br  from-cyan-500    to-sky-200 h-[100vh] w-full">
+        <img
+          src={dor}
+          alt=""
+          className="  object-cover absolute w-full h-full mix-blend-overlay"
+        />
+
+        <div className=" p-20">
+          <h1 className="  text-white text-6xl  font-bold">
+            Hello Rajan Bahadur Shahi
+          </h1>
+          <h1 className=" mt-5  text-white text-3xl font-light">
+            Hello Rajan Bahadur Shahi
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
