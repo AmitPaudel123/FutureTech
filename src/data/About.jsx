0d8db61397be3485b350dd6d00de1e1f791777bb
rajan <rajanshahi1267@gmail.com>
2024-03-01 23:58:31 +0545

    add

--- src/data/About.jsx
+++ src/data/About.jsx
@@ -1,8 +1,15 @@
-import rajan from "../assets/teams/rajan.jpg"
-import loki from "../assets/teams/loki.jpg"
-import gita from "../assets/teams/gita.jpg"
+import rajan from "../assets/teams/rajan.jpg";
+import loki from "../assets/teams/loki.jpg";
+import gita from "../assets/teams/gita.jpg";
+import abhi from "../assets/partner/abhi.jpg";
 
 export const items = [
+  {
+    img: abhi,
+    name: "Abhi Mishra",
+    work: "Founder",
+    desc: "I must explain to you how all this idea of denouncing pleasure.",
+  },
   {
     img: rajan,
     name: "Rajan Bahadur Shahi",
@@ -15,17 +22,11 @@ export const items = [
     work: "Software Teacher.",
     desc: "To take a teachings, which of us ever undertakes physical exercise.",
   },
-  {
-    img: rajan,
-    name: "Hemraj Thapa",
-    work: "Maths Teacher.",
-    desc: "I must explain to you how all this idea of denouncing pleasure.",
-  },
+
   {
     img: gita,
     name: "Gita Bista",
     work: "Dancer Teacher.",
     desc: "The actual teachings of the great explorer of the master-builder.",
   },
-  
 ];
